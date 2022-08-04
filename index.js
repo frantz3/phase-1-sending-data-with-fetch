5e46c2b34696e5fa8d126a66185bca130b9e3dca
frantzmoseau <frantzmoseau35@gmail.com>
2022-08-03 21:57:46 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email) {
+    const data = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({name, email})
+    }
+
+   return fetch('http://localhost:3000/users', data)
+    .then(res => res.json())
+    .then(obj => console.log(obj))
+    .catch(err => {document.body.innerHTML = err.message
+
+    })
+        
+    
+}
